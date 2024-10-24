428a41bc5643c7d606f044556f1fcedb9ce36f54
EL1ASTH <a23310395@live.ceti.mx>
2024-10-24 02:00:04 +0000

    clase 3

--- include/Foco.hpp
+++ include/Foco.hpp
@@ -1,3 +1,8 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
 class Foco
 {
 private:
@@ -10,5 +15,13 @@ public:
     }
     void Apagar(){
         this->encendido = false;
+    }
+    void Imprimir(){
+        if(this->encendido){
+            cout<<"O";
+        }else{
+            cout<<".";
+        }
+        
     }
 };
--- include/Lienzo.hpp
+++ include/Lienzo.hpp
@@ -1,10 +1,25 @@
 #pragma once
+#include <Vector.hpp>
+#include <SerieNavidad.hpp>
+
 
 class Lienzo
 {
 private:
-    
+    Vector posicion;
+    Vector tamanio;
 public:
-    Lienzo() {}
+    Lienzo(Vector p, Vector t) {
+        this->posicion = p;
+        this->tamanio = t;
+    }
     ~Lienzo() {}
+
+    void Dibujar(SerieNavidad serie){
+        serie.Imprimir();
+    }
+
+    void Limpiar(){
+        system("clear");
+    }
 };
